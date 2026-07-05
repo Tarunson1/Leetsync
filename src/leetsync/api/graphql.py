7e590c8ec56915f0f90035fdc1643d9d7f655b50
Tarun Soni <sotarun54@gmail.com>
2026-07-06 03:43:50 +0530

    feat(api): add GraphQL queries

---
+++ src/leetsync/api/graphql.py
@@ -0,0 +1,45 @@
+"""
+GraphQL queries used by the LeetCode client.
+"""
+
+RECENT_SUBMISSIONS_QUERY = """
+query recentAcSubmissionList($username: String!) {
+    recentAcSubmissionList(username: $username) {
+        id
+        title
+        titleSlug
+        timestamp
+    }
+}
+"""
+
+SUBMISSION_DETAILS_QUERY = """
+query submissionDetails($submissionId: Int!) {
+    submissionDetails(submissionId: $submissionId) {
+        runtime
+        runtimeDisplay
+        runtimePercentile
+
+        memory
+        memoryDisplay
+        memoryPercentile
+
+        code
+        timestamp
+        statusCode
+
+        lang {
+            name
+            verboseName
+        }
+
+        question {
+            questionId
+            titleSlug
+        }
+
+        runtimeError
+        compileError
+    }
+}
+"""
