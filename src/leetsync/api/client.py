ef8dee5818f7fd234e85ebdde9f7bf5d7ca6df06
Tarun Soni <sotarun54@gmail.com>
2026-07-07 01:23:01 +0530

    feat(api): implement GraphQL request execution

--- src/leetsync/api/client.py
+++ src/leetsync/api/client.py
@@ -3,6 +3,7 @@ from typing import Any
 from leetsync.config.constants import GRAPHQL_URL
 from leetsync.network.client import HTTPClient
 from leetsync.models.submission import RecentSubmission
+from leetsync.models.submission_details import SubmissionDetails
 from .graphql import RECENT_SUBMISSIONS_QUERY
 
 
@@ -42,6 +43,41 @@ class LeetCodeClient:
         response.raise_for_status()
 
         return response.json()
+    
+    def _map_recent_submission(
+        self,
+        item: dict[str, Any],
+    ) -> RecentSubmission:
+        """
+        Convert a LeetCode API submission into a RecentSubmission model.
+        """
+
+        return RecentSubmission(
+            id=item["id"],
+            title=item["title"],
+            title_slug=item["titleSlug"],
+            timestamp=int(item["timestamp"]),
+        )
+
+    def _map_submission_details(
+    self,
+    item: dict[str, Any],
+) -> SubmissionDetails:
+        """
+    Convert a LeetCode submission details response into a SubmissionDetails model.
+    """
+
+        return SubmissionDetails(
+            code=item["code"],
+            runtime=item["runtimeDisplay"],
+            memory=item["memoryDisplay"],
+            language=item["lang"]["name"],
+            question_id=item["question"]["questionId"],
+            title_slug=item["question"]["titleSlug"],
+            timestamp=int(item["timestamp"]),
+    )
+
+
     def get_recent_submissions(self, username: str) -> list[RecentSubmission]:
         """
     Fetch the user's recent accepted LeetCode submissions.
@@ -62,12 +98,23 @@ class LeetCodeClient:
 
         submissions = response["data"]["recentAcSubmissionList"]
 
+    #     return [
+    #     RecentSubmission(
+    #         id=item["id"],
+    #         title=item["title"],
+    #         title_slug=item["titleSlug"],
+    #         timestamp=int(item["timestamp"]),
+    #     )
+    #     for item in submissions
+    # ]
+
         return [
-        RecentSubmission(
-            id=item["id"],
-            title=item["title"],
-            title_slug=item["titleSlug"],
-            timestamp=int(item["timestamp"]),
-        )
-        for item in submissions
-    ]
+    self._map_recent_submission(item)
+    for item in submissions
+]
+
+    def get_submission_details():
+        pass
+
+    def validate_session():
+        pass
---
+++ src/leetsync/auth/edge.py
@@ -0,0 +1,18 @@
+"""
+Microsoft Edge session provider.
+"""
+
+import browser_cookie3
+
+
+class EdgeSessionProvider:
+    """Loads authenticated cookies from Microsoft Edge."""
+
+    def load(self):
+        """
+        Load LeetCode cookies from Edge.
+
+        Returns:
+            http.cookiejar.CookieJar
+        """
+        return browser_cookie3.edge(domain_name="leetcode.com")
